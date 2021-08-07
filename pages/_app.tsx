2a67e82946b5d28a4e47fb6dc1aa14fe4ef9d559
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-07 23:18:05 +0530

    feat(auth) : add auth0 for authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "../styles/globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
   );
 }
 export default MyApp;
