5cf4e92554b941d2907a6b307cd0b6201ccbf3f6
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-08 09:44:38 +0530

    feat(auth) : make the user stay in the apply page after logging In.
    this change will remove the default behaviour of redirecting user back to home page

--- pages/api/auth/[...auth0].tsx
+++ pages/api/auth/[...auth0].tsx
@@ -1,3 +1,11 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, { returnTo: "http://localhost:3000/apply" });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+});
