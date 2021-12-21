9ad2588206e716aca0049f1792445bd53fb4a17d
hussamkhatib <hussamkhatib20@gmail.com>
2021-12-21 12:20:42 +0530

    upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  purge: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false,
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -21,9 +19,6 @@ module.exports = {
           "rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 12%) 0px 1px 1px 0px, rgb(60 66 87 / 16%) 0px 0px 0px 1px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(60 66 87 / 8%) 0px 3px 9px 0px, rgb(60 66 87 / 8%) 0px 2px 5px 0px",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
