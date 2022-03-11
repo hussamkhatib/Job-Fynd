31d223feb9702485cfdafc91aad9c1e02f80e81e
hussamkhatib <hussamkhatib20@gmail.com>
2022-03-11 15:30:45 +0530

    chore: remove ectending of classes in tailwind config as jit will be used in the future use

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        student: "15rem 1fr",
-      },
-      height: {
-        home: "32rem",
-        studentcard: "max-content",
-      },
-      boxShadow: {
-        input:
-          "rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 12%) 0px 1px 1px 0px, rgb(60 66 87 / 16%) 0px 0px 0px 1px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(60 66 87 / 8%) 0px 2px 5px 0px",
-        inputhover:
-          "rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 12%) 0px 1px 1px 0px, rgb(60 66 87 / 16%) 0px 0px 0px 1px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(60 66 87 / 8%) 0px 3px 9px 0px, rgb(60 66 87 / 8%) 0px 2px 5px 0px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
