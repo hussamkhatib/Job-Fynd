298f1d2fc7246e48dd5ec3102daaf12abae8fe68
hussamkhatib <hussamkhatib20@gmail.com>
2022-03-13 16:40:26 +0530

    chore: add border on left side for larger viewports for NavTabs

--- src/components/NavTabs.tsx
+++ src/components/NavTabs.tsx
@@ -21,15 +21,15 @@ const NavTabs: FC<Props> = ({ tabs }) => {
                     <Link key={tab.name} href={tab.href}>
                         <a className={classNames(
                             isCurrent
-                                ? "border-neutral-900 text-neutral-900"
+                                ? "border-black text-black"
                                 : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                            "group inline-flex items-center border-b-2 py-4 px-1 text-sm font-medium"
+                            "group inline-flex items-center border-b-2 lg:border-b-0 lg:border-l-2 py-4 px-1 text-sm font-medium"
                         )}
                             aria-current={isCurrent ? "page" : undefined}>
                             {tab.icon && (
                                 <tab.icon
                                     className={classNames(
-                                        isCurrent ? "text-neutral-900" : "text-gray-400 group-hover:text-gray-500",
+                                        isCurrent ? "text-black" : "text-gray-400 group-hover:text-gray-500",
                                         "-ml-0.5 hidden h-5 w-5 ltr:mr-2 rtl:ml-2 sm:inline-block"
                                     )}
                                     aria-hidden="true"
