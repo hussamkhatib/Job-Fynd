c16b445aecddb3828abe754a2519d1e944b49c67
hussamkhatib <hussamkhatib20@gmail.com>
2022-03-13 15:50:29 +0530

    fix: make tailwind work in sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,9 @@ module.exports = {
     {
       name: '@storybook/addon-postcss',
       options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
         postcssLoaderOptions: {
           implementation: require('postcss'),
         },
