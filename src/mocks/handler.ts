d7886d2d4a5333b6467585f5c268e1e165b0c8b2
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-02 22:18:17 +0530

    refactor: remove Job and Jobs component as it will no longer be used

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,9 +1,8 @@
 import { rest } from "msw";
-import { jobsHandlers } from "../components/Jobs/jobs.mocks";
 import { sampleStudent1 } from "../components/Table/Table.data";
 
 export const studentsHandlers = [
   rest.get("/student", (req, res, ctx) => res(ctx.json(sampleStudent1))),
 ];
 
-export const handlers = [...jobsHandlers, ...studentsHandlers];
+export const handlers = [...studentsHandlers];
