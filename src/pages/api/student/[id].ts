98dc56ae1aa09a08bff17e0452e656c7dcc78ea1
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-17 10:13:46 +0530

    fix: add default statement for api routes

--- src/pages/api/student/[id].ts
+++ src/pages/api/student/[id].ts
@@ -20,5 +20,10 @@ export default async function userHandler(
       res.status(200).json([result]);
       break;
     }
+    default: {
+      return res
+        .status(405)
+        .json({ message: "Method not allowed", success: false });
+    }
   }
 }
