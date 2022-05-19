b56fe9a2c54056d0c540aaa03ed56d331bbdefc4
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-19 12:51:29 +0530

    feat: add POST request in /api/event to add a event

--- src/pages/api/event/index.ts
+++ src/pages/api/event/index.ts
@@ -22,6 +22,29 @@ export default async function userHandler(
       res.status(200).json(result);
       break;
     }
+    case "POST":
+      {
+        const {
+          company_id,
+          title,
+          ctc,
+          type,
+          last_date_to_apply,
+          branches_allowed,
+        } = req.body;
+        await prisma.event.create({
+          data: {
+            company_id,
+            title,
+            ctc,
+            type,
+            last_date_to_apply,
+            branches_allowed,
+          },
+        });
+      }
+      res.json({ success : true})
+      break;
     default: {
       return res
         .status(405)
