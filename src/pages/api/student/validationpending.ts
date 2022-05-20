22441339592f4ac1bb72027c52887998bda116b9
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-20 16:02:27 +0530

    feat: add api route /student/validationpending which return students whose validation are pending

---
+++ src/pages/api/student/validationpending.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma";
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET": {
+      const result: any = await prisma.student.findMany({
+        where: {
+          validated: "PENDING",
+        },
+      });
+      res.status(200).json(result);
+      break;
+    }
+    default: {
+      return res
+        .status(405)
+        .json({ message: "Method not allowed", success: false });
+    }
+  }
+}
