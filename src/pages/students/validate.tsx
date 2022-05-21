a46b4e6a5f1237eb1c1641795b9df78dd19b7db8
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-21 21:30:27 +0530

    render pending validation students in /students/validate

--- src/pages/students/validate.tsx
+++ src/pages/students/validate.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import NavTabs from "../../components/NavTabs";
 import { studentsTabs } from "../../components/NavTabs/tabs";
+import Table from "../../components/Table";
+import { studentCols } from "../../store/student.data";
 
 const ValidateStudents = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    let res;
+    fetch("/api/student/validation/pending")
+      .then((res) => res.json())
+      .then((data) => {
+        res = [...data];
+        setData(res);
+        setIsLoaded(true);
+      });
+  }, []);
+
   return (
     <div>
       <NavTabs tabs={studentsTabs} />
+      <Table
+        columns={studentCols}
+        rowsCount={10}
+        data={data}
+        isLoading={!isLoaded}
+      />
     </div>
   );
 };
