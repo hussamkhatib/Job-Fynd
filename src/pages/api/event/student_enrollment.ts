95467d01379a6fea34bc2291ef58b7fe4269083b
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-24 16:14:19 +0530

    feat :add POST method in api route to enrol student to a event

---
+++ src/pages/api/event/student_enrollment.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma";
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  // todo: check for last_date_apply ,branches allowed , opted in , minimum marks are satisfied
+  switch (method) {
+    case "POST":
+      {
+        const { student_id, event_id } = req.body;
+        await prisma.student_enrollment.create({
+          data: {
+            student_id: +student_id,
+            event_id: +event_id,
+          },
+        });
+      }
+      res.json({ success: true });
+      break;
+    default: {
+      return res
+        .status(405)
+        .json({ message: "Method not allowed", success: false });
+    }
+  }
+}
