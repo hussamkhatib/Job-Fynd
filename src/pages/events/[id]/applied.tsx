864ac5af0ffca54cf00316258729dc71a61b960e
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-28 10:24:23 +0530

    feat: add event details in /pages/event/[id]/applied

--- src/pages/events/[id]/applied.tsx
+++ src/pages/events/[id]/applied.tsx
@@ -3,28 +3,38 @@ import { useRouter } from "next/router";
 import user, { UserRole } from "../../../userContext";
 import Table from "../../../components/Table";
 import { studentCols } from "../../../store/student.data";
+import { adminEventCols } from "../../../store/events.data";
 
 const EventAppliedPage: FC = () => {
   const userRole = useContext(user);
   const router = useRouter();
   const [data, setData] = useState<any>([]);
+  const [eventData, setEventData] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const { id } = router.query as any;
 
   useEffect(() => {
     id &&
       (async () => {
-        const response = await fetch(`/api/event/${id}/applied`);
+        const response = await fetch(`/api/event/${id}`);
         const json = await response.json();
-        setData(json);
-        setIsLoaded(true);
+        setEventData([json]);
       })();
+    (async () => {
+      const response = await fetch(`/api/event/${id}/applied`);
+      const json = await response.json();
+      setData(json);
+      setIsLoaded(true);
+    })();
   }, [id]);
 
   if (!isLoaded) return <div>loading...</div>;
   if (userRole === UserRole.admin)
     return (
       <div>
+        <h2 className="px-2 py-4 text-xl"> Event Details</h2>
+        <Table columns={adminEventCols} rowsCount={1} data={eventData} />
+        <h2 className="px-2 py-4 text-xl">Applied Students</h2>
         <Table columns={studentCols} rowsCount={10} data={data} />
       </div>
     );
