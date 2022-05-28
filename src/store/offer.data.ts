58f693adecabd059106160bf66c8328c0d19a154
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-28 10:21:19 +0530

    chore: add offerCols

---
+++ src/store/offer.data.ts
@@ -0,0 +1,40 @@
+export const offerCols = [
+  {
+    Header: "Company",
+    accessor: "company",
+  },
+  {
+    Header: "Sector",
+    accessor: "sector",
+  },
+  {
+    Header: "Title",
+    accessor: "title",
+  },
+  {
+    Header: "Type",
+    accessor: "type",
+  },
+  {
+    Header: "CTC",
+    accessor: "ctc",
+  },
+];
+export const studentOfferCols = [
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Branch",
+    accessor: "branch",
+  },
+  {
+    Header: "USN",
+    accessor: "usn",
+  },
+  {
+    Header: "CTC",
+    accessor: "ctc",
+  },
+];
