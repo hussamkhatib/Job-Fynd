ff03db9f65394fbaeb616913c39ec943fe9be865
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-28 14:15:15 +0530

    refactor: delete mock allEvents data that was used for msw

--- src/store/events.data.tsx
+++ src/store/events.data.tsx
@@ -37,28 +37,13 @@ export const eventCols = [
 ];
 export const adminEventCols = [
   ...eventCols,
+   {
+    Header: "Offers",
+    accessor: "offers",
+   } 
   {
     Header: "Offers",
     accessor: "offers",
   },
 ];
-export const allEvents = {
-  data: [
-    {
-      company: "Happiest Minds",
-      ctc: "5.4 LPA",
-      sector: "IT",
-    },
-    {
-      company: "Bygus",
-      ctc: "10 LPA",
-      sector: "Sales",
-    },
-    {
-      company: "Goldman sachs",
-      ctc: "6 LPA",
-      sector: "IT",
-    },
-  ],
-  columns: eventCols,
-};
+
