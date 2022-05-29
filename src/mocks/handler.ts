e74fe7dd357139d5bc398bcdeb4d8fa23d419f21
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-29 21:30:10 +0530

    fix: build errors

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,12 +1,8 @@
 import { rest } from "msw";
-import { allEvents } from "../store/events.data";
 import { sampleStudent } from "../store/student.data";
 
 export const studentsHandlers = [
   rest.get("/student/1", (req, res, ctx) => res(ctx.json(sampleStudent))),
 ];
-export const eventsHandlers = [
-  rest.get("/event", (req, res, ctx) => res(ctx.json(allEvents))),
-];
 
-export const handlers = [...studentsHandlers, ...eventsHandlers];
+export const handlers = [...studentsHandlers];
