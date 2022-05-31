1781532812a54cdc4e83286a18d6e9a7fe2a58e4
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-31 08:13:08 +0530

    feat: add Switch component

---
+++ src/components/ui/Switch/Switch.stories.tsx
@@ -0,0 +1,10 @@
+import Switch from ".";
+
+export default {
+  title: "ui/Switch",
+  component: Switch,
+};
+
+export const TextField = () => (
+  <Switch Lable="Enable Notification" action={() => null} />
+);
