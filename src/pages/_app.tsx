285bbdceaf48ef192ba4580680ed699f2d8b6848
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-01 21:48:14 +0530

    chore: init react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,25 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import AuthGuard from "../components/AuthGuard";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   require("../mocks");
 }
 
+const queryClient = new QueryClient();
+
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <AuthGuard>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <QueryClientProvider client={queryClient}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </AuthGuard>
     </SessionProvider>
   );
