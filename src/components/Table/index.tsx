bd65867a7cf90a922176e4e82d04f04c42ea9abc
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-07 22:34:01 +0530

    feat: upgrade to react-table v8, support pagination

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,60 +1,109 @@
-/* eslint-disable react/jsx-key */
-import { FC } from "react";
-import { useTable } from "react-table";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import {
+  getCoreRowModel,
+  OnChangeFn,
+  PaginationState,
+  useTableInstance,
+} from "@tanstack/react-table";
+import classNames from "classnames";
+import { FC, Fragment } from "react";
 
-interface Props {
-  columns: any[];
-  data: any[];
-}
+type Props = {
+  table: any;
+  columns: any;
+  data: unknown[];
+  manualPagination?: boolean;
+  pageCount?: number;
+  setPagination?: OnChangeFn<PaginationState>;
+  state?: any;
+};
 
-const Table: FC<Props> = ({ columns, data }) => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data,
-      initialState: {
-        hiddenColumns: ["id"],
-      },
-    });
+const Table: FC<Props> = ({
+  table,
+  data,
+  columns,
+  manualPagination = false,
+  pageCount,
+  setPagination,
+  state,
+}) => {
+  const instance = useTableInstance(table, {
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    pageCount: pageCount || undefined,
+    manualPagination,
+    onPaginationChange: setPagination || undefined,
+    state,
+  });
 
   return (
-    <div className="w-full overflow-auto">
-      <table {...getTableProps()}>
-        <thead className="bg-[#F8F9FD]">
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th
-                  {...column.getHeaderProps()}
-                  className="p-2 border-[1px] border-solid"
-                >
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td
-                      {...cell.getCellProps()}
-                      className="p-2 border-[1px] border-solid"
-                    >
-                      {cell.render("Cell")}
-                    </td>
-                  );
-                })}
+    <Fragment>
+      <div className="flex flex-col w-full overflow-auto">
+        <table className="whitespace-nowrap text-ellipsis">
+          <thead className="bg-[#F8F9FD]">
+            {instance.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    className="p-2 border-[1px] border-solid"
+                  >
+                    {header.isPlaceholder ? null : header.renderHeader()}
+                  </th>
+                ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+            ))}
+          </thead>
+          <tbody>
+            {instance.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="p-2 border-[1px] border-solid">
+                    {cell.renderCell()}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {manualPagination && (
+        <div className="flex items-center justify-end gap-2 py-2">
+          <span className="text-sm text-gray-500 ">
+            {instance.getState().pagination.pageIndex + 1}/
+            {instance.getPageCount()}
+          </span>
+          <button
+            onClick={() => instance.previousPage()}
+            disabled={!instance.getCanPreviousPage()}
+          >
+            <ChevronLeftIcon
+              className={classNames(
+                "w-5 h-5 ",
+                instance.getCanPreviousPage()
+                  ? "text-gray-500"
+                  : "text-gray-300"
+              )}
+              aria-hidden="true"
+            />
+          </button>
+          <button
+            onClick={() => instance.nextPage()}
+            disabled={!instance.getCanNextPage()}
+          >
+            <ChevronRightIcon
+              className={classNames(
+                "w-5 h-5 ",
+                instance.getCanNextPage() ? "text-gray-500" : "text-gray-300"
+              )}
+              aria-hidden="true"
+            />
+          </button>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
