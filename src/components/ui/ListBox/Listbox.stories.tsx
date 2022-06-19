d06b368fcc7145a494e678f7f5e525350916aa4c
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-19 13:38:56 +0530

    fix: listbox component it now works for varaints of list types i.e ITEM[] | ITEM | string | string[]

--- src/components/ui/ListBox/Listbox.stories.tsx
+++ src/components/ui/ListBox/Listbox.stories.tsx
@@ -33,3 +33,40 @@ export const Multiple = () => {
     />
   );
 };
+
+const persons = [
+  {
+    id: "1",
+    name: "ROb",
+  },
+  {
+    id: "2",
+    name: "Mary",
+  },
+];
+export const ObjectDefault = () => {
+  const [selected, setSelected] = useState(persons[0]);
+
+  return (
+    <Listbox
+      selected={selected}
+      setSelected={setSelected}
+      list={persons}
+      Label="Choose Person"
+    />
+  );
+};
+
+export const ObjectMultiple = () => {
+  const [selected, setSelected] = useState([persons[0], persons[1]]);
+
+  return (
+    <Listbox
+      selected={selected}
+      setSelected={setSelected}
+      list={persons}
+      Label="Choose Person"
+      multiple
+    />
+  );
+};
