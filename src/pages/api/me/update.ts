7ac1ed56f158448eedc2b594316ede9116eb9494
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-19 13:25:12 +0530

    feat: add /api/me/update to update current user profile

---
+++ src/pages/api/me/update.ts
@@ -0,0 +1,38 @@
+import { Role, Validation } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import prisma from "../../../../lib/prisma";
+import { Session } from "../auth/[...nextauth]";
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  const session = (await getSession({ req })) as never as Session;
+  if (!session) return res.status(403).end();
+  const { user } = session;
+  if (user?.role === Role.admin) res.status(401).end();
+
+  switch (method) {
+    case "PATCH": {
+      const result = await prisma.student.update({
+        where: {
+          email: user?.email,
+        },
+        data: {
+          ...req.body,
+          validated: Validation.pending,
+        },
+      });
+      return res.status(200).json(result);
+    }
+
+    default: {
+      return res
+        .status(405)
+        .json({ message: "Method not allowed", success: false });
+    }
+  }
+}
