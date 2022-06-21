d4ba5c4929a04dc9069c94a220a7e2a9af4378d9
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-21 11:33:26 +0530

    fix: minor fixes

--- src/pages/api/student/[usn]/offers.ts
+++ src/pages/api/student/[usn]/offers.ts
@@ -1,74 +1,58 @@
 import { Role } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import prisma from "../../../../../lib/prisma";
+import { Session } from "../../auth/[...nextauth]";
 
 export default async function userHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const {
-    method,
-    query: { usn },
-  }: any = req;
+  const { method } = req;
 
-  const session: any = await getSession({ req });
+  const session = (await getSession({ req })) as never as Session;
   if (!session) return res.status(403).end();
-  if (usn !== session.user.usn && session.user.role !== Role.admin)
-    return res.status(401).end();
+  if (session.user.role !== Role.admin) return res.status(401).end();
 
   switch (method) {
-    case "GET": {
-      const result: any = await prisma.user.findUnique({
-        where: {
-          usn,
-        },
-        select: {
-          offers: {
-            select: {
-              ctc: true,
-              offer_letter: true,
-              event: {
-                select: {
-                  title: true,
-                  type: true,
-                  company: {
-                    select: {
-                      name: true,
-                      sector: true,
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      });
-      const offers = result.offers.map((item: any) => {
-        return {
-          ctc: item.ctc,
-          company: item.event.company.name,
-          sector: item.event.company.sector,
-          offer_letter: item.offer_letter,
-          title: item.event.title,
-          type: item.event.type,
-        };
-      });
-      res.status(200).json(offers);
-      break;
-    }
-    case "POST": {
-      const { ctc, offer_letter, event_id } = req.body;
-      await prisma.offer.create({
-        data: {
-          ctc,
-          offer_letter,
-          event_id,
-          student_usn: session.user.usn,
-        },
-      });
-      return res.status(201).end();
-    }
+    // case "GET": {
+    //   const result: any = await prisma.user.findUnique({
+    //     where: {
+    //       usn :session.user.usn,
+    //     },
+    //     select: {
+    //       offers: {
+    //         select: {
+    //           ctc: true,
+    //           offer_letter: true,
+    //           event: {
+    //             select: {
+    //               title: true,
+    //               type: true,
+    //               company: {
+    //                 select: {
+    //                   name: true,
+    //                   sector: true,
+    //                 },
+    //               },
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   });
+    //   const offers = result.offers.map((item: any) => {
+    //     return {
+    //       ctc: item.ctc,
+    //       company: item.event.company.name,
+    //       sector: item.event.company.sector,
+    //       offer_letter: item.offer_letter,
+    //       title: item.event.title,
+    //       type: item.event.type,
+    //     };
+    //   });
+    //   return res.status(200).json(offers);
+    // }
+
     default: {
       return res
         .status(405)
