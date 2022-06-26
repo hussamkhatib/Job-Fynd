048da561b8fd3958962813f31be5d3b45463de6b
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-26 07:40:54 +0530

    fix: delete duplicate component for Modal

--- src/components/ui/Modal/Modal.stories.tsx
+++ src/components/ui/Modal/Modal.stories.tsx
@@ -1,27 +1,30 @@
 import { Fragment, useState } from "react";
-import Modal from ".";
 import Button from "../Button";
+import ButtonGroup from "../Button/ButtonGroup";
+import Modal from ".";
 
 export default {
   title: "ui/Modal",
   component: Modal,
 };
 
-const action = () => {
-  return undefined;
-};
 export const Default = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <Fragment>
-      <Modal
-        title="Deactivate"
-        content="Are you sure you want to deactivate your account? All of your data will be permanently
-  removed. This action cannot be undone."
-        action={action}
-        state={{ open, setOpen }}
-      />
+      <Modal title="Deactivate" state={{ open, setOpen }}>
+        <p className="text-sm text-gray-500">
+          Are you sure you want to deactivate your account? All of your data
+          will be permanently removed. This action cannot be undone.
+        </p>
+        <ButtonGroup className="px-4 py-3 space-x-4 sm:px-6 sm:flex sm:flex-row-reverse">
+          <Button onClick={() => setOpen(false)}>Delete</Button>
+          <Button color="secondary" onClick={() => setOpen(false)}>
+            Cancel
+          </Button>
+        </ButtonGroup>
+      </Modal>
       <Button onClick={() => setOpen(true)}>Open</Button>
     </Fragment>
   );
--- src/components/ui/Modal/index.tsx
+++ src/components/ui/Modal/index.tsx
@@ -1,32 +1,21 @@
-import { FC } from "react";
-import { Fragment, useRef } from "react";
+import { FC, ReactNode } from "react";
+import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationIcon } from "@heroicons/react/outline";
 
 interface Props {
   title: string;
-  content: string;
-  action: () => void;
   state: {
     open: boolean;
-    setOpen: any;
+    setOpen: (value: boolean) => void;
   };
+  children: ReactNode;
 }
-const Modal: FC<Props> = ({ title, content, action, state }) => {
+
+const Modal: FC<Props> = ({ children, state, title }) => {
   const { open, setOpen } = state;
-  const handleAction = () => {
-    action();
-    setOpen(false);
-  };
-  const cancelButtonRef = useRef(null);
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={setOpen}
-      >
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -53,41 +42,16 @@ const Modal: FC<Props> = ({ title, content, action, state }) => {
               <Dialog.Panel className="relative overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:max-w-lg sm:w-full">
                 <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationIcon
-                        className="w-6 h-6 text-red-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <div className="w-full mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                       <Dialog.Title
                         as="h3"
                         className="text-lg font-medium leading-6 text-gray-900"
                       >
                         {title}
                       </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">{content}</p>
-                      </div>
+                      <div className="mt-2">{children}</div>
                     </div>
                   </div>
-                </div>
-                <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
-                  <button
-                    onClick={() => handleAction()}
-                    type="button"
-                    className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  >
-                    Delete
-                  </button>
-                  <button
-                    type="button"
-                    className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
-                  >
-                    Cancel
-                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
