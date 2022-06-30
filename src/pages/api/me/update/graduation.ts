1514abdb803f3e991eeeb6ae73fce5f9d94e52d9
hussamkhatib <hussamkhatib20@gmail.com>
2022-06-30 15:57:13 +0530

    feat: add edit graduation and diploma options

---
+++ src/pages/api/me/update/graduation.ts
@@ -0,0 +1,36 @@
+import { Role } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import prisma from "../../../../../lib/prisma";
+import { Session } from "../../auth/[...nextauth]";
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  const session = (await getSession({ req })) as never as Session;
+  if (!session) return res.status(403).end();
+  const { user } = session;
+  if (user?.role === Role.admin) res.status(401).end();
+
+  switch (method) {
+    case "PATCH": {
+      const result = await prisma.graduation.upsert({
+        where: {
+          recordId: user.email,
+        },
+        update: { ...req.body, recordId: user.email },
+        create: { ...req.body, recordId: user.email },
+      });
+      return res.status(200).json(result);
+    }
+
+    default: {
+      return res
+        .status(405)
+        .json({ message: "Method not allowed", success: false });
+    }
+  }
+}
--- src/pages/profile/documents/edit.tsx
+++ src/pages/profile/documents/edit.tsx
@@ -1,7 +1,7 @@
 import { ExternalLinkIcon, PencilIcon } from "@heroicons/react/solid";
 import axios from "axios";
 import React, { FormEvent, Fragment, useRef, useState } from "react";
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import { toast } from "react-toastify";
 import NavTabs from "../../../components/NavTabs";
 import { profileTabs } from "../../../components/NavTabs/tabs";
@@ -52,6 +52,8 @@ const DocumentsForm = () => {
     <div className="max-w-xl mx-auto">
       <EditSslc sslc={data?.sslc} />
       <EditPuc puc={data?.puc} />
+      <EditDiploma diploma={data?.diploma} />
+      <EditGraduation graduation={data?.graduation} />
     </div>
   );
 };
@@ -64,12 +66,15 @@ const EditSslc = ({ sslc }: any) => {
   const [board, setBoard] = useState(sslc?.board);
   const [scoreType, setScoreType] = useState(sslc?.scoreType);
   const scoreRef = useRef<HTMLInputElement>(null!);
+  const queryClient = useQueryClient();
+
   const { mutate, isLoading } = useMutation(
     (values: any) => axios.patch(`/api/me/update/sslc`, values),
     {
       onSettled: (data, error) => {
         if (data) {
           toast.success("Profile Updated");
+          queryClient.invalidateQueries(["studentProfile", "?profile=full"]);
           setOpen(false);
         }
         if (error instanceof Error) toast.error(`Error: ${error.message}`);
@@ -164,12 +169,15 @@ const EditPuc = ({ puc }: any) => {
   const [board, setBoard] = useState(puc?.board);
   const [scoreType, setScoreType] = useState(puc?.scoreType);
   const scoreRef = useRef<HTMLInputElement>(null!);
+  const queryClient = useQueryClient();
+
   const { mutate, isLoading } = useMutation(
     (values: any) => axios.patch(`/api/me/update/puc`, values),
     {
       onSettled: (data, error) => {
         if (data) {
           toast.success("Profile Updated");
+          queryClient.invalidateQueries(["studentProfile", "?profile=full"]);
           setOpen(false);
         }
         if (error instanceof Error) toast.error(`Error: ${error.message}`);
@@ -240,6 +248,414 @@ const EditPuc = ({ puc }: any) => {
             name="score"
             id="score"
             label="Score"
+          />
+          <ButtonGroup className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse sm:space-x-reverse ">
+            <Button color="primary" type="submit" loading={isLoading}>
+              Save Details
+            </Button>
+            <Button
+              type="button"
+              color="secondary"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+          </ButtonGroup>
+        </form>
+      </Modal>
+    </Fragment>
+  );
+};
+
+const EditDiploma = ({ diploma }: any) => {
+  const [open, setOpen] = useState(false);
+  const sem1Ref = useRef<HTMLInputElement>(null!);
+  const sem2Ref = useRef<HTMLInputElement>(null!);
+  const sem3Ref = useRef<HTMLInputElement>(null!);
+  const sem4Ref = useRef<HTMLInputElement>(null!);
+  const sem5Ref = useRef<HTMLInputElement>(null!);
+  const sem6Ref = useRef<HTMLInputElement>(null!);
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading } = useMutation(
+    (values: any) => axios.patch(`/api/me/update/diploma`, values),
+    {
+      onSettled: (data, error) => {
+        if (data) {
+          toast.success("Profile Updated");
+          queryClient.invalidateQueries(["studentProfile", "?profile=full"]);
+          setOpen(false);
+        }
+        if (error instanceof Error) toast.error(`Error: ${error.message}`);
+      },
+    }
+  );
+  const updateDiplomaHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const sem1 = sem1Ref?.current?.value;
+    const sem2 = sem2Ref?.current?.value;
+    const sem3 = sem3Ref?.current?.value;
+    const sem4 = sem4Ref?.current?.value;
+    const sem5 = sem5Ref?.current?.value;
+    const sem6 = sem6Ref?.current?.value;
+    mutate({
+      sem1,
+      sem2,
+      sem3,
+      sem4,
+      sem5,
+      sem6,
+    });
+  };
+
+  return (
+    <Fragment>
+      <section className="my-9">
+        <div className="grid space-x-2 grid-cols-[8rem_max-content] my-2 items-center">
+          <h3 className="text-lg">Diploma</h3>
+          <Button
+            size="sm"
+            color="minimal"
+            StartIcon={PencilIcon}
+            onClick={() => setOpen(true)}
+          />
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 1</div>
+          <div className="flex flex-1 text-gray-700">
+            {diploma?.sem1}
+            {diploma?.sem1MarksSheet && (
+              <a href={diploma?.sem1MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 2</div>
+          <div className="flex flex-1 text-gray-700">
+            {diploma?.sem2}
+            {diploma?.sem2MarksSheet && (
+              <a href={diploma?.sem2MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 3</div>
+          <div className="flex flex-1 text-gray-700">
+            {diploma?.sem3}
+            {diploma?.sem3MarksSheet && (
+              <a href={diploma?.sem3MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 4</div>
+          <div className="flex flex-1 text-gray-700">
+            {diploma?.sem4}
+            {diploma?.sem4MarksSheet && (
+              <a href={diploma?.sem4MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 5</div>
+          <div className="flex flex-1 text-gray-700">
+            {diploma?.sem5}
+            {diploma?.sem5MarksSheet && (
+              <a href={diploma?.sem5MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 6</div>
+          <div className="flex flex-1 text-gray-700">
+            {diploma?.sem6}
+            {diploma?.sem6MarksSheet && (
+              <a href={diploma?.sem6MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+      </section>
+      <Modal title="SSLC" state={{ open, setOpen }}>
+        <form onSubmit={updateDiplomaHandler}>
+          <TextField
+            ref={sem1Ref}
+            defaultValue={diploma?.sem1}
+            name="sem1"
+            id="sem1"
+            label="Sem 1"
+          />
+          <TextField
+            ref={sem2Ref}
+            defaultValue={diploma?.sem2}
+            name="sem2"
+            id="sem2"
+            label="Sem 2"
+          />
+          <TextField
+            ref={sem3Ref}
+            defaultValue={diploma?.sem3}
+            name="sem3"
+            id="sem3"
+            label="Sem 3"
+          />
+          <TextField
+            ref={sem4Ref}
+            defaultValue={diploma?.sem4}
+            name="sem4"
+            id="sem4"
+            label="Sem 4"
+          />
+          <TextField
+            ref={sem5Ref}
+            defaultValue={diploma?.sem5}
+            name="sem5"
+            id="sem5"
+            label="Sem 5"
+          />
+          <TextField
+            ref={sem6Ref}
+            defaultValue={diploma?.sem6}
+            name="sem6"
+            id="sem6"
+            label="Sem 6"
+          />
+          <ButtonGroup className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse sm:space-x-reverse ">
+            <Button color="primary" type="submit" loading={isLoading}>
+              Save Details
+            </Button>
+            <Button
+              type="button"
+              color="secondary"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+          </ButtonGroup>
+        </form>
+      </Modal>
+    </Fragment>
+  );
+};
+
+const EditGraduation = ({ graduation }: any) => {
+  const [open, setOpen] = useState(false);
+  const sem1Ref = useRef<HTMLInputElement>(null!);
+  const sem2Ref = useRef<HTMLInputElement>(null!);
+  const sem3Ref = useRef<HTMLInputElement>(null!);
+  const sem4Ref = useRef<HTMLInputElement>(null!);
+  const sem5Ref = useRef<HTMLInputElement>(null!);
+  const sem6Ref = useRef<HTMLInputElement>(null!);
+  const sem7Ref = useRef<HTMLInputElement>(null!);
+  const sem8Ref = useRef<HTMLInputElement>(null!);
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading } = useMutation(
+    (values: any) => axios.patch(`/api/me/update/graduation`, values),
+    {
+      onSettled: (data, error) => {
+        if (data) {
+          toast.success("Profile Updated");
+          queryClient.invalidateQueries(["studentProfile", "?profile=full"]);
+          setOpen(false);
+        }
+        if (error instanceof Error) toast.error(`Error: ${error.message}`);
+      },
+    }
+  );
+  const updateGraduationHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const sem1 = sem1Ref?.current?.value;
+    const sem2 = sem2Ref?.current?.value;
+    const sem3 = sem3Ref?.current?.value;
+    const sem4 = sem4Ref?.current?.value;
+    const sem5 = sem5Ref?.current?.value;
+    const sem6 = sem6Ref?.current?.value;
+    const sem7 = sem6Ref?.current?.value;
+    const sem8 = sem6Ref?.current?.value;
+    mutate({
+      sem1,
+      sem2,
+      sem3,
+      sem4,
+      sem5,
+      sem6,
+      sem7,
+      sem8,
+    });
+  };
+
+  return (
+    <Fragment>
+      <section className="my-9">
+        <div className="grid space-x-2 grid-cols-[8rem_max-content] my-2 items-center">
+          <h3 className="text-lg">Graduation</h3>
+          <Button
+            size="sm"
+            color="minimal"
+            StartIcon={PencilIcon}
+            onClick={() => setOpen(true)}
+          />
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 1</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem1}
+            {graduation?.sem1MarksSheet && (
+              <a href={graduation?.sem1MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 2</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem2}
+            {graduation?.sem2MarksSheet && (
+              <a href={graduation?.sem2MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 3</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem3}
+            {graduation?.sem3MarksSheet && (
+              <a href={graduation?.sem3MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 4</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem4}
+            {graduation?.sem4MarksSheet && (
+              <a href={graduation?.sem4MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 5</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem5}
+            {graduation?.sem5MarksSheet && (
+              <a href={graduation?.sem5MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 6</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem6}
+            {graduation?.sem6MarksSheet && (
+              <a href={graduation?.sem6MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 7</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem7}
+            {graduation?.sem7MarksSheet && (
+              <a href={graduation?.sem7MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="grid space-x-2 grid-cols-[8rem_1fr] my-2">
+          <div className="text-gray-400 ">SEM 8</div>
+          <div className="flex flex-1 text-gray-700">
+            {graduation?.sem8}
+            {graduation?.sem8MarksSheet && (
+              <a href={graduation?.sem8MarksSheet}>
+                <ExternalLinkIcon className="w-5 h-5" aria-hidden />
+              </a>
+            )}
+          </div>
+        </div>
+      </section>
+      <Modal title="SSLC" state={{ open, setOpen }}>
+        <form onSubmit={updateGraduationHandler}>
+          <TextField
+            ref={sem1Ref}
+            defaultValue={graduation?.sem1}
+            name="sem1"
+            id="sem1"
+            label="Sem 1"
+          />
+          <TextField
+            ref={sem2Ref}
+            defaultValue={graduation?.sem2}
+            name="sem2"
+            id="sem2"
+            label="Sem 2"
+          />
+          <TextField
+            ref={sem3Ref}
+            defaultValue={graduation?.sem3}
+            name="sem3"
+            id="sem3"
+            label="Sem 3"
+          />
+          <TextField
+            ref={sem4Ref}
+            defaultValue={graduation?.sem4}
+            name="sem4"
+            id="sem4"
+            label="Sem 4"
+          />
+          <TextField
+            ref={sem5Ref}
+            defaultValue={graduation?.sem5}
+            name="sem5"
+            id="sem5"
+            label="Sem 5"
+          />
+          <TextField
+            ref={sem6Ref}
+            defaultValue={graduation?.sem6}
+            name="sem6"
+            id="sem6"
+            label="Sem 6"
+          />
+          <TextField
+            ref={sem7Ref}
+            defaultValue={graduation?.sem7}
+            name="sem7"
+            id="sem7"
+            label="Sem 7"
+          />
+          <TextField
+            ref={sem8Ref}
+            defaultValue={graduation?.sem8}
+            name="sem8"
+            id="sem8"
+            label="Sem 8"
           />
           <ButtonGroup className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse sm:space-x-reverse ">
             <Button color="primary" type="submit" loading={isLoading}>
