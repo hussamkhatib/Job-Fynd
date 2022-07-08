b667557c7c020fcba6f1d4e3c4dc3146e4a9606e
hussamkhatib <hussamkhatib20@gmail.com>
2022-07-08 19:59:48 +0530

    fix: overlapping of Combobox

--- src/components/ui/Combobox/index.tsx
+++ src/components/ui/Combobox/index.tsx
@@ -1,9 +1,9 @@
 import { FC, Fragment, useEffect, useState } from "react";
-import { Combobox, Transition } from "@headlessui/react";
+import { Combobox as HeadlessuiCombobox, Transition } from "@headlessui/react";
 import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
 import Props, { Item } from "./Combobox.types";
 
-const MyCombobox: FC<Props> = ({
+const Combobox: FC<Props> = ({
   getFilteredList,
   selected,
   setSelected,
@@ -21,21 +21,21 @@ const MyCombobox: FC<Props> = ({
   }, [getFilteredList, query]);
 
   return (
-    <Combobox value={selected} onChange={setSelected} name={Label}>
-      <Combobox.Label>{Label}</Combobox.Label>
+    <HeadlessuiCombobox value={selected} onChange={setSelected} name={Label}>
+      <HeadlessuiCombobox.Label>{Label}</HeadlessuiCombobox.Label>
       <div className="relative mt-1">
         <div className="relative w-full overflow-hidden text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-300 sm:text-sm">
-          <Combobox.Input
+          <HeadlessuiCombobox.Input
             className="w-full py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 border-none focus:ring-0"
             displayValue={(item: Item) => item?.name}
             onChange={(event) => setQuery(event.target.value)}
           />
-          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+          <HeadlessuiCombobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
             <SelectorIcon
               className="w-5 h-5 text-gray-400"
               aria-hidden="true"
             />
-          </Combobox.Button>
+          </HeadlessuiCombobox.Button>
         </div>
         <Transition
           as={Fragment}
@@ -44,14 +44,14 @@ const MyCombobox: FC<Props> = ({
           leaveTo="opacity-0"
           afterLeave={() => setQuery("")}
         >
-          <Combobox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <HeadlessuiCombobox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {filteredList.length === 0 && query !== "" ? (
               <div className="relative px-4 py-2 text-gray-700 cursor-default select-none">
                 Nothing found.
               </div>
             ) : (
               filteredList.map((item: Item) => (
-                <Combobox.Option
+                <HeadlessuiCombobox.Option
                   key={item.id}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
@@ -80,14 +80,14 @@ const MyCombobox: FC<Props> = ({
                       ) : null}
                     </>
                   )}
-                </Combobox.Option>
+                </HeadlessuiCombobox.Option>
               ))
             )}
-          </Combobox.Options>
+          </HeadlessuiCombobox.Options>
         </Transition>
       </div>
-    </Combobox>
+    </HeadlessuiCombobox>
   );
 };
 
-export default MyCombobox;
+export default Combobox;
