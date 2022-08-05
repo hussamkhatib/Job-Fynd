7205f2095e9a64b9664842623da520d82c60284a
hussamkhatib <hussamkhatib@gmail.com>
2022-08-05 07:22:58 +0530

    chroe: return info when sendMail func

--- src/utils/utils.server.ts
+++ src/utils/utils.server.ts
@@ -1,5 +1,5 @@
 import cloudinary from "cloudinary";
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,31 +14,30 @@ const uploadFile = async (file: string) =>
 
 export { uploadFile };
 
-export async function sendMail(to: string | string[], subject: string, text: string) {
-  const transporter = nodemailer.createTransport({  
+export async function sendMail(
+  to: string | string[],
+  subject: string,
+  text: string
+) {
+  const transporter = nodemailer.createTransport({
     service: "gmail",
     port: 465,
     secure: true,
-    auth: {   
-      user: process.env.EMAIL_USER, 
-      pass: process.env.EMAIL_PASSWORD
- 
-    }
-  })
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
   const mailOptions = {
-    from: process.env.EMAIL_USER, 
+    from: process.env.EMAIL_USER,
     to,
     subject,
-    text
+    text,
   };
-    // send mail with defined transport object
-    const info = await transporter.sendMail(mailOptions);
-  
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  // send mail with defined transport object
+  const info = await transporter.sendMail(mailOptions);
 
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  return info;
 }
