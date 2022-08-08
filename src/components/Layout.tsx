5ccb4eaaffa41bb594d6088c5868607772248e9f
hussamkhatib <hussamkhatib@gmail.com>
2022-08-08 20:56:32 +0530

    style: add min-h-screen to all viewports

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
   return (
-    <main className="md:grid md:grid-flow-col md:grid-cols-[max-content_1fr] md:h-screen bg-gray-100">
+    <main className="md:grid md:grid-flow-col md:grid-cols-[max-content_1fr] h-screen bg-gray-100">
       <NavBar />
       <div className="px-4 overflow-auto">{children}</div>
       <BottomNavBar />
