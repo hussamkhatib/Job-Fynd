f30c3506b0e70c1e9228616ae80360e3d902d929
hussamkhatib <hussamkhatib@gmail.com>
2022-08-10 11:00:11 +0530

    perf: lazy load BranchWiseOffers this will only be shipped for  admin

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,18 @@
 import { Role } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import BranchWiseOffers from "../components/BranchWiseOffers";
+import { lazy, Suspense } from "react";
+import Loader from "../components/ui/Loader";
+// import BranchWiseOffers from "../components/BranchWiseOffers";
 
 const Dashboard = () => {
   const { data: session } = useSession();
   if (session?.user.role === Role.student) return null;
+  const BranchWiseOffers = lazy(() => import("../components/BranchWiseOffers"));
   return (
     <div>
-      <BranchWiseOffers />
+      <Suspense fallback={<Loader />}>
+        <BranchWiseOffers />
+      </Suspense>
     </div>
   );
 };
